b3d2dcbc0bdaa38782593d8c7cc11302baf027db
Paolo Viotti <skypixel@gmail.com>
2021-02-21 22:12:32 +0000

    feat: add company profile model

---
+++ Model.fs
@@ -0,0 +1,15 @@
+module Model
+
+type CompanyProfile =
+    { country: string
+      currency: string
+      exchange: string
+      ipo: string
+      marketCapitalization: float
+      name: string
+      phone: string
+      shareOutstanding: float
+      ticker: string
+      weburl: string
+      logo: string
+      finnhubIndustry: string }
