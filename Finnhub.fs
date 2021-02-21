737b99e9fbb99232895e0d40e34b7dd99a75e084
Paolo Viotti <skypixel@gmail.com>
2021-02-21 22:14:06 +0000

    feat: deserialize json using System.Text.Json

--- Finnhub.fs
+++ Finnhub.fs
@@ -1,14 +1,19 @@
 ﻿//namespace FinnhubDotNet
 
+module FinnhubDotNet
+
 open Hopac
 open HttpFs.Client
 open HttpFs.Client.Request
+open System.Text.Json
+
+open Model
 
 type Client(key: string) =
     let key = key
     let BaseUrl = "https://finnhub.io/api/v1/"
 
-    let request path (parameters: list<string * string>) =
+    member _.request<'T> path (parameters: list<string * string>) =
         let baseRequest =
             createUrl Get (BaseUrl + path)
             |> queryStringItem "token" key
@@ -18,13 +23,16 @@ type Client(key: string) =
         |> List.fold (|>) baseRequest
         |> responseAsString
         |> run
+        |> JsonSerializer.Deserialize<'T>
 
-    member _.CompanyProfile(symbol: string) =
-        request "stock/profile2" [ ("symbol", symbol) ]
+    member this.CompanyProfile(symbol: string) =
+        this.request<CompanyProfile> "stock/profile2" [ ("symbol", symbol) ]
 
 
 [<EntryPoint>]
 let main _argv =
     let client = Client("dummy")
-    printfn "%s" (client.CompanyProfile "AAPL")
+    let companyProfile = client.CompanyProfile "AAPL"
+    printfn "%s" companyProfile.exchange
+    printfn "%A" companyProfile
     0
