111e8f4f4e6f5b10b24255121619cb710c63e3dc
Paolo Viotti <skypixel@gmail.com>
2021-02-21 19:48:38 +0000

    feat: base functional methods for requests

--- Finnhub.fs
+++ Finnhub.fs
@@ -1,9 +1,30 @@
-﻿namespace FinnhubDotNet
+﻿//namespace FinnhubDotNet
+
+open Hopac
+open HttpFs.Client
+open HttpFs.Client.Request
 
 type Client(key: string) =
     let key = key
+    let BaseUrl = "https://finnhub.io/api/v1/"
 
-    member this.GetQuote() =
-        0
+    let request path (parameters: list<string * string>) =
+        let baseRequest =
+            createUrl Get (BaseUrl + path)
+            |> queryStringItem "token" key
+
+        parameters
+        |> List.map (fun x -> queryStringItem (fst x) (snd x))
+        |> List.fold (|>) baseRequest
+        |> responseAsString
+        |> run
+
+    member _.CompanyProfile(symbol: string) =
+        request "stock/profile2" [ ("symbol", symbol) ]
 
 
+[<EntryPoint>]
+let main _argv =
+    let client = Client("dummy")
+    printfn "%s" (client.CompanyProfile "AAPL")
+    0
